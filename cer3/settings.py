d4f108fe8d23d759b831d7d390fc8d78a69af75a
chupvoid <chupvoi1703@gmail.com>
2024-07-04 21:26:07 -0400

    Agregando el container react

--- cer3/settings.py
+++ cer3/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
 
     'backend',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4400',
+    'http://127.0.0.1:4400'
+]
