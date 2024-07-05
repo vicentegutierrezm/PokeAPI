fcf66bb60ebdbab8d7e1d34163f2dbf939eb709a
chupvoid <chupvoi1703@gmail.com>
2024-07-05 18:19:01 -0400

    Corriendo react con imagenes locales

--- react/src/App.js
+++ react/src/App.js
@@ -4,6 +4,18 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+// Función para importar todas las imágenes de la carpeta pokemon_images
+function importAll(r) {
+  let images = {};
+  r.keys().forEach(key => {
+    images[key.replace('./', '')] = r(key);
+  });
+  return images;
+}
+
+// Importa todas las imágenes de la carpeta media/pokemon_images
+const images = importAll(require.context('/app/src/pokemon_images', false, /\.(png)$/));
+
 const App = () => {
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -13,7 +25,6 @@ const App = () => {
     const fetchPokemons = async () => {
       try {
         const response = await axios.get('http://localhost:8080/pokemons/');
-        console.log(response);
         setPokemons(response.data);
         setLoading(false);
       } catch (error) {
@@ -28,6 +39,8 @@ const App = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error fetching data</p>;
 
+  console.log(images);
+
   return (
     <div className="App">
       <h1>Pokémon List</h1>
@@ -37,7 +50,8 @@ const App = () => {
             <h2>{pokemon.name}</h2>
             <p>Primary Type: {pokemon.primary_type}</p>
             <p>Secondary Type: {pokemon.secondary_type}</p>
-            <img src={pokemon.image_url} alt={pokemon.name} />
+            {/* Construye dinámicamente la ruta de la imagen */}
+            <img src={images[`${pokemon.name}.png`]} alt={pokemon.name} />
           </li>
         ))}
       </ul>
